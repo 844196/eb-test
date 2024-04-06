b8b80a5fb9609dcb31d759972daeac8e619bdb7d
844196 <844196@users.noreply.github.com>
2024-04-06 10:12:54 +0900

    wip

--- docker/app/docker-entrypoint.live.sh
+++ docker/app/docker-entrypoint.live.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-# aws ssm get-parameter \
-#   --name /eb-test/${ENVIRONMENT}/secret \
-#   --query Parameter.Value \
-#   --output text \
-#   > /opt/app/secret
+aws ssm get-parameter \
+  --name /eb-test/${ENVIRONMENT}/secret \
+  --query Parameter.Value \
+  --output text \
+  > /opt/app/secret
 
 # "docker-php-entrypoint" はベースイメージで定義されているエントリーポイント
 exec docker-php-entrypoint "$@"
